a655162e87225b2792f895fd284493f9aa243c29
Julio Caicedo <llulioscesar@gmail.com>
2025-01-15 21:09:52 -0500

    Feat:front-components Agrega componentes en moleculas

---
+++ ecommerce/src/components/elements/index.ts
@@ -0,0 +1,7 @@
+'use client';
+
+export {default as CreditCart} from './credit-card';
+export {default as NavbarCard} from './navbar-cart';
+export {default as NavbarLink} from './navbar-link';
+export {default as NavbarSearch} from './navbar-search';
+export {default as NavbarToggle} from './navbar-toggle';
